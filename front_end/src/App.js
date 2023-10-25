d83afb7b576e73797d81d81c543a58a1807f397a
張祥賢 <johnny890122>
2023-10-26 00:26:18 +0800

    add frontend

---
+++ front_end/src/App.js
@@ -0,0 +1,45 @@
+import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+
+const App = () => { 
+    const [testingData, setTestingData] = useState([]);
+    useEffect(() => {
+        axios.get("http://localhost:8000/api/")
+           .then((response) => response)
+           .then((data) => {
+                setTestingData(data["data"]["test"]);
+           })
+           .catch((err) => {
+              console.log(err.message);
+           });
+     }, []);
+
+    return (
+    <div class="otree-body container">
+        <h2 class="otree-title page-header">Survey</h2>
+        <form class="otree-form" method="post" role="form" id="form" autocomplete="off">
+            <input type="hidden" name="csrfmiddlewaretoken" value="XrBNGM0QHb1P0dLEcfI6CvA1kS0W00m4lCGX2TyZ5pw9QgVqQmVUh67lbtpEdcLs"/>
+            <div class="_otree-content">
+                <p>
+                    Please answer the following questions. 
+                </p>
+
+                <p>{testingData}</p>
+
+                <div class="form-group required">
+                    <label class="col-form-label" for="id_age">What is your age?</label>
+                    <div class="controls  field-age">
+                        <input type="number" name="age" min="13" max="125" required id="id_age" class="form-control"/>
+                    </div>
+                </div>
+                <p>
+                    <button class="otree-btn-next btn btn-primary">Next</button>
+                </p>
+            </div>
+        </form>
+    </div>
+    )
+}
+
+export default App;
