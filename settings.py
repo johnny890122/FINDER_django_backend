5dd443251e08119e0e47dbf14678f46abe5809c2
張祥賢 <johnny890122>
2023-11-11 01:32:26 +0800

    test: deploy old otree

--- settings.py
+++ settings.py
@@ -137,9 +137,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "frontend/build/static"),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "frontend/build/static"),
+# ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
