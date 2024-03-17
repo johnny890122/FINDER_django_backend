08ccb356ad74f9edbc2eb9e32478edfff9fec923
Xiang Xian, Zhang <j087061439@gmail.com>
2024-03-17 16:23:51 +0800

    test: test deploy

--- settings.py
+++ settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
