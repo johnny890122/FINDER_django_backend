82ccf3ccdbe28b56d6f34273eaf02c7050a799a3
Xiang Xian, Zhang <j087061439@gmail.com>
2024-04-21 12:03:39 +0800

    Revert "test: for gcc"
    This reverts commit d3a88c36222f63df0ff3cfe59e88e673c7b909da.

--- game/FINDER/make.sh
+++ game/FINDER/make.sh
@@ -1,2 +1,4 @@
 python game/FINDER/setup.py build_ext -i
 mv *.so game/FINDER/
+mv *.cpp game/FINDER/
+mv *.c game/FINDER/
--- game/FINDER/setup.py
+++ game/FINDER/setup.py
@@ -1,19 +1,15 @@
 from distutils.core import setup
-from setuptools import find_packages
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
-
 setup(
     cmdclass = {'build_ext':build_ext},
-    packages=find_packages(include=['game', 'Cython', 'pyximport']),
     ext_modules = [
-                    Extension('PrepareBatchGraph', sources = ['FINDER/PrepareBatchGraph.pyx','FINDER/src/lib/PrepareBatchGraph.cpp','FINDER/src/lib/graph.cpp','FINDER/src/lib/graph_struct.cpp',  'FINDER/src/lib/disjoint_set.cpp'], language='c++', extra_compile_args=['-std=c++11']),
-                    Extension('graph', sources=['FINDER/graph.pyx', 'FINDER/src/lib/graph.cpp'], language='c++',extra_compile_args=['-std=c++11']),
-                    Extension('mvc_env', sources=['FINDER/mvc_env.pyx', 'FINDER/src/lib/mvc_env.cpp', 'FINDER/src/lib/graph.cpp','FINDER/src/lib/disjoint_set.cpp'], language='c++',extra_compile_args=['-std=c++11']),
-                    Extension('utils', sources=['FINDER/utils.pyx', 'FINDER/src/lib/utils.cpp', 'FINDER/src/lib/graph.cpp', 'FINDER/src/lib/graph_utils.cpp', 'FINDER/src/lib/disjoint_set.cpp', 'FINDER/src/lib/decrease_strategy.cpp'], language='c++',extra_compile_args=['-std=c++11']),
-                    Extension('nstep_replay_mem', sources=['FINDER/nstep_replay_mem.pyx', 'FINDER/src/lib/nstep_replay_mem.cpp', 'FINDER/src/lib/graph.cpp', 'FINDER/src/lib/mvc_env.cpp', 'FINDER/src/lib/disjoint_set.cpp'], language='c++',extra_compile_args=['-std=c++11']),
-                    Extension('nstep_replay_mem_prioritized',sources=['FINDER/nstep_replay_mem_prioritized.pyx', 'FINDER/src/lib/nstep_replay_mem_prioritized.cpp','FINDER/src/lib/graph.cpp', 'FINDER/src/lib/mvc_env.cpp', 'FINDER/src/lib/disjoint_set.cpp'], language='c++',extra_compile_args=['-std=c++11']),
-                    Extension('graph_struct', sources=['FINDER/graph_struct.pyx', 'FINDER/src/lib/graph_struct.cpp'], language='c++',extra_compile_args=['-std=c++11']),
-                    Extension('FINDER', sources = ['FINDER/FINDER.pyx'])
-                   ]
-    )
+                    Extension('PrepareBatchGraph', sources = ['game/FINDER/PrepareBatchGraph.pyx','game/FINDER/src/lib/PrepareBatchGraph.cpp','game/FINDER/src/lib/graph.cpp','game/FINDER/src/lib/graph_struct.cpp',  'game/FINDER/src/lib/disjoint_set.cpp'], language='c++', extra_compile_args=['-std=c++11']),
+                   Extension('graph', sources=['game/FINDER/graph.pyx', 'game/FINDER/src/lib/graph.cpp'], language='c++',extra_compile_args=['-std=c++11']),
+                    Extension('mvc_env', sources=['game/FINDER/mvc_env.pyx', 'game/FINDER/src/lib/mvc_env.cpp', 'game/FINDER/src/lib/graph.cpp','game/FINDER/src/lib/disjoint_set.cpp'], language='c++',extra_compile_args=['-std=c++11']),
+                    Extension('utils', sources=['game/FINDER/utils.pyx', 'game/FINDER/src/lib/utils.cpp', 'game/FINDER/src/lib/graph.cpp', 'game/FINDER/src/lib/graph_utils.cpp', 'game/FINDER/src/lib/disjoint_set.cpp', 'game/FINDER/src/lib/decrease_strategy.cpp'], language='c++',extra_compile_args=['-std=c++11']),
+                    Extension('nstep_replay_mem', sources=['game/FINDER/nstep_replay_mem.pyx', 'game/FINDER/src/lib/nstep_replay_mem.cpp', 'game/FINDER/src/lib/graph.cpp', 'game/FINDER/src/lib/mvc_env.cpp', 'game/FINDER/src/lib/disjoint_set.cpp'], language='c++',extra_compile_args=['-std=c++11']),
+                    Extension('nstep_replay_mem_prioritized',sources=['game/FINDER/nstep_replay_mem_prioritized.pyx', 'game/FINDER/src/lib/nstep_replay_mem_prioritized.cpp','game/FINDER/src/lib/graph.cpp', 'game/FINDER/src/lib/mvc_env.cpp', 'game/FINDER/src/lib/disjoint_set.cpp'], language='c++',extra_compile_args=['-std=c++11']),
+                    Extension('graph_struct', sources=['game/FINDER/graph_struct.pyx', 'game/FINDER/src/lib/graph_struct.cpp'], language='c++',extra_compile_args=['-std=c++11']),
+                    Extension('FINDER', sources = ['game/FINDER/FINDER.pyx'])
+                   ])
