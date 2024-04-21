d30970b2171e0c0c40b320292d05d06b69160813
johnny890122 <j087061439@gmail.com>
2024-04-21 15:14:48 +0800

    test

--- game/FINDER/setup.py
+++ game/FINDER/setup.py
@@ -4,8 +4,8 @@ from Cython.Distutils import build_ext
 setup(
     cmdclass = {'build_ext':build_ext},
     ext_modules = [
-                    Extension('PrepareBatchGraph', sources = ['PrepareBatchGraph.pyx','FINDER/src/lib/PrepareBatchGraph.cpp','FINDER/src/lib/graph.cpp','FINDER/src/lib/graph_struct.cpp',  'FINDER/src/lib/disjoint_set.cpp'], language='c++', extra_compile_args=['-std=c++11']),
-                    Extension('graph', sources=['FINDER/graph.pyx', 'FINDER/src/lib/graph.cpp'], language='c++',extra_compile_args=['-std=c++11']),
+                    Extension('PrepareBatchGraph', sources = ['PrepareBatchGraph.pyx','src/lib/PrepareBatchGraph.cpp','src/lib/graph.cpp','src/lib/graph_struct.cpp',  'src/lib/disjoint_set.cpp'], language='c++', extra_compile_args=['-std=c++11']),
+                    Extension('graph', sources=['graph.pyx', 'src/lib/graph.cpp'], language='c++',extra_compile_args=['-std=c++11']),
                     Extension('mvc_env', sources=['FINDER/mvc_env.pyx', 'FINDER/src/lib/mvc_env.cpp', 'FINDER/src/lib/graph.cpp','FINDER/src/lib/disjoint_set.cpp'], language='c++',extra_compile_args=['-std=c++11']),
                     Extension('utils', sources=['FINDER/utils.pyx', 'FINDER/src/lib/utils.cpp', 'FINDER/src/lib/graph.cpp', 'FINDER/src/lib/graph_utils.cpp', 'FINDER/src/lib/disjoint_set.cpp', 'FINDER/src/lib/decrease_strategy.cpp'], language='c++',extra_compile_args=['-std=c++11']),
                     Extension('nstep_replay_mem', sources=['FINDER/nstep_replay_mem.pyx', 'FINDER/src/lib/nstep_replay_mem.cpp', 'FINDER/src/lib/graph.cpp', 'FINDER/src/lib/mvc_env.cpp', 'FINDER/src/lib/disjoint_set.cpp'], language='c++',extra_compile_args=['-std=c++11']),
