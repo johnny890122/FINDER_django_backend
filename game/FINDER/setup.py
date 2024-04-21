0bb5ff04117db0441304ebed718bfcbad237a413
Xiang Xian, Zhang <j087061439@gmail.com>
2024-04-21 12:05:06 +0800

    test: the gcc issue on another machine

--- game/FINDER/setup.py
+++ game/FINDER/setup.py
@@ -5,7 +5,7 @@ setup(
     cmdclass = {'build_ext':build_ext},
     ext_modules = [
                     Extension('PrepareBatchGraph', sources = ['game/FINDER/PrepareBatchGraph.pyx','game/FINDER/src/lib/PrepareBatchGraph.cpp','game/FINDER/src/lib/graph.cpp','game/FINDER/src/lib/graph_struct.cpp',  'game/FINDER/src/lib/disjoint_set.cpp'], language='c++', extra_compile_args=['-std=c++11']),
-                   Extension('graph', sources=['game/FINDER/graph.pyx', 'game/FINDER/src/lib/graph.cpp'], language='c++',extra_compile_args=['-std=c++11']),
+                    Extension('graph', sources=['game/FINDER/graph.pyx', 'game/FINDER/src/lib/graph.cpp'], language='c++',extra_compile_args=['-std=c++11']),
                     Extension('mvc_env', sources=['game/FINDER/mvc_env.pyx', 'game/FINDER/src/lib/mvc_env.cpp', 'game/FINDER/src/lib/graph.cpp','game/FINDER/src/lib/disjoint_set.cpp'], language='c++',extra_compile_args=['-std=c++11']),
                     Extension('utils', sources=['game/FINDER/utils.pyx', 'game/FINDER/src/lib/utils.cpp', 'game/FINDER/src/lib/graph.cpp', 'game/FINDER/src/lib/graph_utils.cpp', 'game/FINDER/src/lib/disjoint_set.cpp', 'game/FINDER/src/lib/decrease_strategy.cpp'], language='c++',extra_compile_args=['-std=c++11']),
                     Extension('nstep_replay_mem', sources=['game/FINDER/nstep_replay_mem.pyx', 'game/FINDER/src/lib/nstep_replay_mem.cpp', 'game/FINDER/src/lib/graph.cpp', 'game/FINDER/src/lib/mvc_env.cpp', 'game/FINDER/src/lib/disjoint_set.cpp'], language='c++',extra_compile_args=['-std=c++11']),
